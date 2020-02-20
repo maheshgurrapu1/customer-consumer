b5035ee1934ce6ae734506e15a5dff96a9c951c8
Mahesh Gurrapu <maheshg537@gmail.com>
2020-02-20 11:34:24 +0530

    Using Json Converter

--- src/main/java/com/prokarma/customer/consumer/impl/CustomerServiceImpl.java
+++ src/main/java/com/prokarma/customer/consumer/impl/CustomerServiceImpl.java
@@ -1,9 +1,8 @@
 package com.prokarma.customer.consumer.impl;
 
 import org.springframework.stereotype.Service;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.prokarma.customer.consumer.domain.Customer;
+import com.prokarma.customer.consumer.common.JsonConverter;
+import com.prokarma.customer.consumer.entity.Customer;
 import com.prokarma.customer.consumer.repository.CustomerRepository;
 import com.prokarma.customer.consumer.service.CustomerService;
 
@@ -11,16 +10,17 @@ import com.prokarma.customer.consumer.service.CustomerService;
 public class CustomerServiceImpl implements CustomerService {
 
   private CustomerRepository customerRepository;
+  private JsonConverter jsonConverter;
 
-  public CustomerServiceImpl(CustomerRepository customerRepository) {
+  public CustomerServiceImpl(CustomerRepository customerRepository, JsonConverter jsonConverter) {
     this.customerRepository = customerRepository;
+    this.jsonConverter = jsonConverter;
   }
 
 
   @Override
-  public void save(String customerJson) throws JsonProcessingException { // handle the exception
-                                                                         // throw RuntimeException
-    Customer customer = new ObjectMapper().readValue(customerJson, Customer.class);
+  public void save(String customerJson) {
+    Customer customer = jsonConverter.fromJson(customerJson, Customer.class);
     customerRepository.saveAndFlush(customer);
 
   }
